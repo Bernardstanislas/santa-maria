27247b29283f710d45e015945a4ce61236b4cdda
Stanislas Bernard <bernardstanislas@gmail.com>
2019-07-18 17:55:15 +0200

    :tada: Empty Laravel app

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: vendor/bin/rr get && ./rr serve -v -d -o http.address=:$PORT
--- psr-worker.php
+++
@@ -1,5 +0,0 @@
-<?php
-require __DIR__ . "/vendor/autoload.php";
-
-$bridge = new \updg\roadrunner\laravel\Bridge();
-$bridge->start();
